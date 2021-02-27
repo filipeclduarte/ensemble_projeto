910471949183350796319a2d6c3e8a2a671407e4
Filipe Duarte <filipe_pb_duarte@hotmail.com>
2021-02-27 17:09:27 -0300

    add sliding window pred

--- main.py
+++ main.py
@@ -18,36 +18,51 @@ df_sf = pd.read_csv('dados/df_santa_fe.csv')
 # normalização gaussiana
 scaler = preprocessing.StandardScaler().fit(df_sf['x'].values.reshape(-1,1))
 df = scaler.transform(df_sf['x'].values.reshape(-1,1))
-
+n_in = 10
 ## treinamento as 1000 primeiras obs
 treino = df[:1000]
 teste = df[1000:]
+X_teste_pred = df[(1000-n_in):].T# pegando as 10 últimas obs de treinamento
 
 # Estruturar os dados
 ## transformar o problema de série em supervised learning
 ### testando com 10 obs passadas
-df_treino_sup = series_to_supervised(treino.reshape(-1,1), n_in=10)
+df_treino_sup = series_to_supervised(treino.reshape(-1,1), n_in=n_in)
 X_treino = df_treino_sup.drop(columns='var1(t)').values
 Y_treino = df_treino_sup['var1(t)'].values
 
-df_teste_sup = series_to_supervised(teste.reshape(-1,1), n_in=10)
+df_teste_sup = series_to_supervised(teste.reshape(-1,1), n_in=n_in)
 X_teste = df_teste_sup.drop(columns='var1(t)').values
 Y_teste = df_teste_sup['var1(t)'].values
 
 # Treinar ELM
 ## Testando com 10 neurônios na camada escondida
-elm = ELMRegressor(10)
+elm = ELMRegressor(n_in)
 elm.fit(X_treino, Y_treino)
 
 ## pred
-Y_pred = elm.predict(X_teste)
+#TODO: para teste faço a previsão e incorpor na série de teste para prever os próximos etc
+# primeira pred
+Y_pred = np.empty(Y_teste.shape)
+Y_pred[0] = elm.predict(X_teste_pred[:,:n_in])
+
+for i in range(1,Y_teste.shape[0]):
+    # andar a janela
+    X_teste_temp = X_teste_pred[:,i:i+n_in]
+    if i == 1:
+        print('X_teste_temp.shape = ', X_teste_temp.shape)
+
+    X_teste_temp[:, -1] = Y_pred[i-1] # para a quantidade de obs de testee
+    Y_pred[i] = elm.predict(X_teste_temp)
+
+# Y_pred = elm.predict(X_teste)
 ## RMSE de teste
-RMSE = mean_squared_error(Y_teste, Y_pred, squared = True)
+RMSE = mean_squared_error(Y_teste, Y_pred.reshape(-1,1), squared = True)
 print(f'RMSE = {RMSE}')
 
 ## Plotar 
 plt.plot(Y_teste, label='Real')
-plt.plot(Y_pred, label='Prediction')
+plt.plot(Y_pred.reshape(-1,1), label='Prediction')
 plt.legend()
 plt.title('Teste')
 plt.show()
